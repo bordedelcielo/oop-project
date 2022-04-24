618523f405e8fea18efa50ac94a4b8a765e80f5b
bordedelcielo <christopher.thrutchley@gmail.com>
2022-04-23 23:59:35 -0500

    theme chosen (FromSoftware bosses)

--- program.py
+++ program.py
@@ -1,1 +1,9 @@
 print('hello world')
+
+class fromSoftwareBoss():
+    def __init__(self, name, game, image):
+        self.name = name
+        self.game = game
+        self.image = image
+
+fromSoftwareBoss('Slave Knight Gael','The Ringed City')
